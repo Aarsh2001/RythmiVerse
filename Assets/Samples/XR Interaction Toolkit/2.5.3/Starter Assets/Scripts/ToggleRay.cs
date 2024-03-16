ecabedd4371c7299b9943fd91ce79528b3bf23d8
Aarsh2001 <ac5973@srmist.edu.in>
2024-03-16 20:02:53 +0000

    jiaming's latest changes

--- Assets/Samples/XR Interaction Toolkit/2.5.3/Starter Assets/Scripts/ToggleRay.cs
+++ Assets/Samples/XR Interaction Toolkit/2.5.3/Starter Assets/Scripts/ToggleRay.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 
 public class ToggleRay : MonoBehaviour
