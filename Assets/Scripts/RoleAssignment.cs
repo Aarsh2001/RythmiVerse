bb922cbc247cc6e06c2123db3e59ee37e700eb09
b92981du <dennis.utu@student.manchester.ac.uk>
2024-03-20 00:52:11 +0000

    menu to test on headset

---
+++ Assets/Scripts/RoleAssignment.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Ubiq.Messaging;
+using Ubiq.Geometry;
+
+public class RoleAssignment : MonoBehaviour
+{
+    // This method can be directly called by the XR Interaction event
+    NetworkContext context;
+
+    private void Start()
+    {
+        context = NetworkContext.Register(this);
+        ConnectionManagerEditor = RoleManager.Instance;
+
+    }
+    public void AssignRoleBasedOnXRInteractor(XRBaseInteractor interactor)
+    {
+        if (gameObject.CompareTag("TeacherButton"))
+        {
+            AssignTeacherRole();
+        }
+        else if (gameObject.CompareTag("StudentButton"))
+        {
+            AssignStudentRole();
+        }
+    }
+
+    private void AssignTeacherRole()
+    {
+        Debug.Log("Assigned as Teacher");
+        // Additional teacher role assignRment logic here
+    }
+
+    private void AssignStudentRole()
+    {
+        Debug.Log("Assigned as Student");
+        // Additional student role assignment logic here
+    }
+}
---
+++ Assets/Scripts/RoleManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ubiq.Messaging;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    NetworkContext context;
+    public static RoleManager Instance { get; private set; }
+
+    public string role; //string for roles
+
+    // Use this for initialization
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // Initialize isOwner here or elsewhere as needed
+            role = "";
+        }
+        else
+        {
+            Destroy(gameObject); // Ensures there is only one instance
+        }
+    }
+
+    // Update is called once per frame
+    public void UpdateRole(string role)
+    {
+        role = role;
+    }
+}
