f729159cfdab041dd1f359863b1d8f2ccc5f1bd1
Aarsh2001 <ac5973@srmist.edu.in>
2024-03-21 19:11:16 +0000

    replay sequence

--- Assets/Scripts/ReplayManager.cs
+++ Assets/Scripts/ReplayManager.cs
@@ -7,9 +7,9 @@ public class ReplayManager : MonoBehaviour
     // Trigger this method however you like (e.g., with a UI button press)
     public void TriggerReplay()
     {
-        if (playerCollision.Instance.sequence != null)
-        {
+       
+        
             StartCoroutine(playerCollision.ReplayCollisionSequence(30f));
-        }
+        
     }
 }
